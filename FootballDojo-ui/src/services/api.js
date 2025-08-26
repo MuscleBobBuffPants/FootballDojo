3275c07516ee10f93d3cb282e421f98f5d0274a8
MuscleBobBuffPants <prokesj12@gmail.com>
2025-08-26 12:17:47 -0400

    Add environment files for url

--- FootballDojo-ui/src/services/api.js
+++ FootballDojo-ui/src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
+console.log(API_BASE); // should print your URL
+
 export const api = axios.create({
-    baseURL: 'https://localhost:5000/api/',
+    baseURL: API_BASE
 });
