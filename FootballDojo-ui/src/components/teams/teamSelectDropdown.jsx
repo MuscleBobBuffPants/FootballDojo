4a23048f50907631fa1b9ec121ef888e705fdd82
MuscleBobBuffPants <prokesj12@gmail.com>
2025-08-28 14:24:37 -0400

    Revert "Get scrollbar to appear for dropdowns on mobile"
    This reverts commit d69367d4dcee0eb7e1835062d95b4113b5e885df.

--- FootballDojo-ui/src/components/teams/teamSelectDropdown.jsx
+++ FootballDojo-ui/src/components/teams/teamSelectDropdown.jsx
@@ -20,12 +20,10 @@ function TeamSelectDropdown({ teamsByLeagueId, selectedTeam, handleTeamChange })
                     value={isNonEmptyObject(selectedTeam) ? selectedTeam : ""}
                     label="Team"
                     onChange={handleTeamChange}
-                    native={false} 
                     MenuProps={{
                         PaperProps: {
                             style: {
                                 maxHeight: 48 * 5 + 8, // 5 items at 48px height + padding
-                                overflowY: "auto"
                             },
                         },
                     }}
