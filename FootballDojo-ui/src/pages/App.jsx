001fe94c2b3a8e74d6cedb260ea9cb000d2ab024
Joe Prokes <prokesj12@gmail.com>
2025-08-30 22:06:11 -0400

    Clean up title (WIP)

--- FootballDojo-ui/src/pages/App.jsx
+++ FootballDojo-ui/src/pages/App.jsx
@@ -1,5 +1,5 @@
 import { DarkMode, LightMode } from "@mui/icons-material";
-import { CssBaseline, IconButton, ThemeProvider, Typography } from "@mui/material";
+import { Box, CssBaseline, IconButton, ThemeProvider, Typography } from "@mui/material";
 import { useState } from "react";
 import Home from '../components/home';
 import { getTheme } from '../global/theme';
@@ -12,26 +12,26 @@ function App() {
     };
 
     return (
-        <>
-            <ThemeProvider theme={getTheme(mode)}>
-                <CssBaseline />
-                <div style={{ display: "flex" }}>
-                    <IconButton onClick={toggleMode} color="inherit">
-                        {mode === "light" ? <LightMode /> : <DarkMode />}
-                    </IconButton>
-                </div>
-                <div style={{ textAlign: "center" }}>
-                    <Typography variant="h4" component="h2" sx={{ pb: 3 }}>
-                        {"F\u00fatbol"} Dojo
-                    </Typography>
-                </div>
-                <Home />
-                <Typography component="h2" sx={{ pt: 12 }}>
+        <ThemeProvider theme={getTheme(mode)}>
+            <CssBaseline />
+            <Box width="100%" display="flex" justifyContent="flex-start" p={2}>
+                <IconButton onClick={toggleMode} color="inherit">
+                    {mode === "light" ? <LightMode /> : <DarkMode />}
+                </IconButton>
+            </Box>
+            <Box width="100%" textAlign="center">
+                <Typography variant="h4" component="h2" sx={{ pb: 3 }}>
+                    {"F\u00fatbol"} Dojo
+                </Typography>
+            </Box>
+            <Home />
+            <Box width="100%" textAlign="center" sx={{ pt: 12 }}>
+                <Typography component="h2">
                     2025 {"F\u00fatbol"} Dojo. All Rights Reserved. Made & Operated by MuscleBobBuffPants.
                 </Typography>
-            </ThemeProvider>
-        </>
-    )
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;
